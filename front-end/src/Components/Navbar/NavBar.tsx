ed4f0b79a24c1efcee9b828a5baa687de5fbd594
krishnabakshi2455 <krishnabakshi2455@gmail.com>
2024-11-18 21:35:41 +0530

    navbar-structure,post-component-css

--- front-end/src/Components/Navbar/NavBar.tsx
+++ front-end/src/Components/Navbar/NavBar.tsx
@@ -6,7 +6,7 @@ import ChatIcon from '@mui/icons-material/Chat';
 import { useSelector } from 'react-redux';
 import './Navbar.css';
 import { RootState } from '../../ReduxFolder/store'; // Assuming RootState is defined in the Redux store
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import Notification from '../Notification/Notification';
 
 
@@ -18,28 +18,50 @@ const Navbar: React.FC = () => {
         <>
             <div className='navbar-main-div'>
 
-                
-                    <div className='navbar-container' >
 
-                        <div className='icon-container '>
-                            <Link to="/" className='nav-css  '><HomeIcon  /></Link>
-                        <Link to="/Posts" className='nav-css  '><ChatIcon /></Link>
-                        <Link to="/Posts" className='nav-css  '><Notification /></Link>
-                        <Link to="/Create" className='nav-css '><AddCircleOutlineIcon /></Link>
-                        <Link to='/profile'><Avatar className='w-2 special-icon-1'>{SingUpUsername ? SingUpUsername[0] : 'U'}</Avatar></Link>
-                        </div>
+                <div className='navbar-container' >
 
-                        <div className='navbar-name-container'>
-                        <Link to="/" className='nav-css  '>Home</Link>
+                    <div className='icon-container '>
+                        <NavLink
+                            to="/"
+                            className={({ isActive }) => isActive ? 'nav-activecss' : 'flex items-center gap-4'}>
+                            <HomeIcon />
+                            <span className='nav-text'>Home</span>
+                        </NavLink>
 
-                        <Link to="/chats" className='nav-css '>Chats</Link>
-                        <Link to="/notification" className='nav-css '>Notification</Link>
-                        <Link to="/Create" className='nav-css '>Create</Link>
-                        <Link to="/profile" className='nav-css '>{SingUpUsername}</Link>
-                        </div>
+                        <NavLink
+                            to="/posts"
+                            className={({ isActive }) => isActive ? 'nav-activecss' : 'flex items-center gap-4'}>
+                            <ChatIcon />
+                            <span className='nav-text'>Chats</span>
+                        </NavLink>
 
+                        <NavLink
+                            to="/notification"
+                            className={({ isActive }) => isActive ? 'nav-activecss' : 'flex items-center gap-4'}>
+                            <Notification />
+                            <span className='nav-text'>Notification</span>
+                        </NavLink>
+
+                        <NavLink
+                            to="/create"
+                            className={({ isActive }) => isActive ? 'nav-activecss' : 'flex items-center gap-4'}>
+                            <AddCircleOutlineIcon />
+                            <span className='nav-text'>Create</span>
+                        </NavLink>
+
+                        <NavLink
+                            to="/profile"
+                            className={({ isActive }) => isActive ? 'nav-activecss' : 'flex items-center gap-4'}>
+                            <Avatar className="w-2 special-icon-1">
+                                {SingUpUsername ? SingUpUsername[0] : 'U'}
+                            </Avatar>
+                            <span className='nav-text'>{SingUpUsername}</span>
+                        </NavLink>
                     </div>
-        
+
+                </div>
+
             </div>
         </>
     );
